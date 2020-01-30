0a669635e9b1676d37052a0f9a2865816a87b9af
Caio Dottori <caio.dottori@starkbank.com>
2020-01-30 20:15:09 -0300

    added EllipticCurve module

---
+++ lib/ellipticcurve.ex
@@ -0,0 +1,5 @@
+defmodule EllipticCurve do
+  @moduledoc """
+  Pure Elixir package to generate and read private/public key pairs, sign and verify messages using the Elliptic Curve Digital Signature Algorithmn (ECDSA).
+  """
+end
