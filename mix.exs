e193fe399629f0fbd4bfbf5cdb8b66d71bc23115
Caio Dottori <caio.dottori@starkbank.com>
2020-02-04 16:45:36 -0300

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,31 @@ defmodule Ellipticcurve.MixProject do
   def project do
     [
       app: :starkbank_ecdsa,
+      name: :starkbank_ecdsa,
       version: "0.0.1",
+      homepage_url: "https://starkbank.com",
+      source_url: "https://github.com/starkbank/ecdsa-elixir",
+      description: description(),
       elixir: "~> 1.9",
-      deps: deps()
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package()
     ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Stark Bank"],
+      licenses: [:MIT],
+      links: %{
+        "StarkBank" => "https://starkbank.com",
+        "GitHub" => "https://github.com/starkbank/ecdsa-elixir"
+      }
+    ]
+  end
+
+  defp description do
+    "A lightweight and fast pure Elixir ECDSA library."
   end
 
   def application do
@@ -15,6 +36,8 @@ defmodule Ellipticcurve.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 end
